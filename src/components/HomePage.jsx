14e3da7eaa3e01420b1f2170f46a6c98fe5b4972
Tushar Thakur <tushar.brandclever@gmail.com>
2024-05-16 18:24:33 +0530

    first init

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+export default function HomePage() {
+    return (
+        <div className='landing_page_main'>
+            <div className='container-fluid'>
+                <div className='Start_top_section'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-md-7'>
+                                <div className='left_side_banner_text'>
+                                    <h4>HELLO, MY NAME IS</h4>
+                                    <h5>Keira Bullock</h5>
+                                    <p>A FREELANCE WEB DEVE |</p>
+                                    
+                                </div>
+                            </div>  
+                            <div className='col-md-5'>
+                                <div className='banner_right_side_img'>
+                                    <img alt="banner img" src='../images/banner.png'></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
