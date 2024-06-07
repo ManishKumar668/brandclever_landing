b9d5a73561944b7ccd4f83c33bc3fa1686cb7534
deepak <brandclever.deepak2@gmail.com>
2024-06-07 12:43:11 +0530

    free

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
+import blogs from '../data/Blog.json'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 export default function Blog() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        arrows: false,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
     return (
         <div>
-
             <section className='about_story padding_top_down slider_section blog_sec'>
                 <Container>
                     <div className='about_us_section_main_start'>
@@ -20,86 +57,37 @@ export default function Blog() {
                     </div>
                     <div className='blog_main_section'>
                         <div className="row">
-                            <div className="col-md-4 set_blogs_responsive">
-                                <div className="blog-item">
-                                    <div className="blog-img">
-                                        <a href="#">
-                                            <img src="https://pxdraft.com/themeforest/keira/keira/static/img/blog-img2.jpg" title="" alt="" />
-                                        </a>
-                                        {/* <div className="post-meta">
-                                            By <a href="">John Doe</a> <span>03.02.2018</span>
-                                        </div> */}
-                                    </div>
-                                    <div className="blog-content">
-                                        <h4 className="font-alt">
-                                            <a href="#">Top 20 HTML Websites.</a>
-                                        </h4>
-                                        <p>
-                                            I design and develop services for customers of all sizes, specializing
-                                            in creating stylish, modern websites, web services and online stores and digital marketing.
-                                        </p>
-                                        <a className="theme-bg more" href="#">
-                                            Read More
-                                        </a>
-                                    </div>
-                                    {/* /blog-content */}
-                                </div>{" "}
-                                {/* Blog Item */}
-                            </div>
-                            <div className="col-md-4 set_blogs_responsive">
-                                <div className="blog-item">
-                                    <div className="blog-img">
-                                        <a href="#">
-                                            <img src="https://pxdraft.com/themeforest/keira/keira/static/img/blog-img4.jpg" title="" alt="" />
-                                        </a>
-                                        {/* <div className="post-meta">
-                                            By <a href="">John Doe</a> <span>03.02.2018</span>
-                                        </div> */}
-                                    </div>
-                                    <div className="blog-content">
-                                        <h4 className="font-alt">
-                                            <a href="#">Easy way to create website.</a>
-                                        </h4>
-                                        <p>
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                            velit esse.
-                                        </p>
-                                        <a className="theme-bg more" href="#">
-                                            Read More
-                                        </a>
-                                    </div>
-                                    {/* /blog-content */}
-                                </div>{" "}
-                                {/* Blog Item */}
-                            </div>
-                            <div className="col-md-4  set_blogs_responsive">
-                                <div className="blog-item">
-                                    <div className="blog-img">
-                                        <a href="#">
-                                            <img src="https://pxdraft.com/themeforest/keira/keira/static/img/blog-img3.jpg" title="" alt="" />
-                                        </a>
-                                        {/* <div className="post-meta">
-                                            By <a href="">John Doe</a> <span>03.02.2018</span>
-                                        </div> */}
-                                    </div>
-                                    <div className="blog-content">
-                                        <h4 className="font-alt">
-                                            <a href="#">Log in to your hosting account.</a>
-                                        </h4>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                            minim veniam.
-                                        </p>
-                                        <a className="theme-bg more" href="#">
-                                            Read More
-                                        </a>
-                                    </div>
-                                    {/* /blog-content */}
-                                </div>{" "}
-                                {/* Blog Item */}
-                            </div>
+                            <Slider {...settings}>
+                                {
+                                    blogs.data.map((ele, key) => {
+                                        return (
+                                            <div className="col-md-4 set_blogs_responsive">
+                                                <div className="blog-item">
+                                                    <div className="blog-img">
+                                                        <a href="#">
+                                                            <img src={ele.image} title="" alt={ele.title1} className='blog_img'/>
+                                                        </a>
+
+                                                    </div>
+                                                    <div className="blog-content">
+                                                        <h4 className="font-alt">
+                                                            <a href="#">{ele.title1}</a>
+                                                        </h4>
+                                                        <p>
+                                                            {ele.introduction.substring(0, 180)}...
+                                                        </p>
+                                                        <a className="theme-bg more" href="#">
+                                                            Read More
+                                                        </a>
+                                                    </div>
+                                                    {/* /blog-content */}
+                                                </div>{" "}
+                                                {/* Blog Item */}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Slider>
                         </div>
 
                     </div>
